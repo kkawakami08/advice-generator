530aef4175ebc0526772378d039c32672a33abc7
Kaori Kawakami <k.kawakami08@gmail.com>
2023-04-09 09:38:42 -0700

    desktop layout

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import diceImage from '../public/images/icon-dice.svg'
 export default function Home() {
   return (
     <main className="bg-darkBlue h-screen flex items-center justify-center">
-      <div className="bg-darkGrayishBlue w-11/12 rounded-xl flex items-center flex-col py-10 px-7 relative gap-7 pb-16">
+      <div className="bg-darkGrayishBlue w-11/12 rounded-xl flex items-center flex-col py-10 px-7 relative gap-7 pb-16 md:w-[30rem]">
         <p className="text-neonGreen text-xs tracking-[.3rem]">ADVICE #117</p>
         <p className="text-lightCyan text-2xl text-center leading-8">"It is easy to sit up and take notice, what's difficult is getting up and taking action."</p>
         <div className="">
